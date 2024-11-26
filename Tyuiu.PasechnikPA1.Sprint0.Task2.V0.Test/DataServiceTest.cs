634539c3884f2c7265b810bb90523bebb64f520b
Pavel Pasechnik <ingle1337@gmail.com>
2024-11-26 17:23:36 +0500

    Task2

---
+++ Tyuiu.PasechnikPA1.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.PasechnikPA1.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.PasechnikPA1.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ChechGetMessageValid()
+        {
+            var name = "Павел";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Павел", res);
+        }
+    }
+}
---
+++ Tyuiu.PasechnikPA1.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.PasechnikPA1.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.PasechnikPA1.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Павел"));
+            Console.ReadKey();
+        }
+    }
+}
