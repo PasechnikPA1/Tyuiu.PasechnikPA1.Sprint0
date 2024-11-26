129e75c8871ce9f44e355b51e91590884e74139c
Pavel Pasechnik <ingle1337@gmail.com>
2024-11-26 15:36:52 +0500

    Task1

---
+++ Tyuiu.PasechnikPA1.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tyuiu.PasechnikPA1.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Павел";
+            int age = 21;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " год! ");
+            Console.ReadKey();
+        }
+    }
+}
