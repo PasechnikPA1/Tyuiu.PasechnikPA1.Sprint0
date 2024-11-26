5aa7cf7f9f545f5c0990d2fa81f1793d8e1620b1
Pavel Pasechnik <ingle1337@gmail.com>
2024-11-26 17:41:57 +0500

    Task3

---
+++ Tyuiu.PasechnikPA1.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.PasechnikPA1.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.PasechnikPA1.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadLine();
+        }
+    }
+}
